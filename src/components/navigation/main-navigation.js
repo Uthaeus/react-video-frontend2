0621137b9972fd6dd4b87965af6097b1f7ea09ee
Uthaeus <romanlavery@gmail.com>
2023-06-18 13:53:34 -0600

    created root layout, homepage, and main navigation components

---
+++ src/components/navigation/main-navigation.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+function MainNavigation() {
+
+    return (
+        <div className="main-navigation">
+            <div>
+
+            </div>
+
+            <div>
+                <NavLink to="/" end>Home</NavLink>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default MainNavigation;
---
+++ src/pages/homepage.js
@@ -0,0 +1,10 @@
+
+function Homepage() {
+  return (
+    <div className="homepage">
+        <h1>Welcome to my Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
